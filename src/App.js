b90f5dd5dd9e11b50deb33945c05e9d23ecd0979
Jianni Hu <jh2585@cornell.edu>
2023-04-06 14:29:43 -0400

    create a react app

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Todo from "./Components/todo";
+import TodoForm from "./Components/form";
+import React from "react";
+
+function App() {
+  const [todos, setTodos] = React.useState([
+    {
+      text: "learn react",
+      isCompleted: false,
+    },
+    {
+      text: "meet friend for lunch",
+      isCompleted: false,
+    },
+    {
+      text: "build todo app",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo = (text) => {
+    const newTodos = [...todos, { text, isCompleted: false }];
+    setTodos(newTodos);
+  };
+  const removeTodo = (index) => {
+    let temp = [...todos];
+    temp.splice(index, 1);
+    setTodos(temp);
+  };
+
+  return (
+    <div className="app">
+      <div className="todo-list">
+        {todos.map((todo, i) => (
+          <Todo key={i} index={i} todo={todo} remove={removeTodo} />
+        ))}
+        <TodoForm addTodo={addTodo} />
+      </div>
+    </div>
+  );
+}
+
+export default App;
